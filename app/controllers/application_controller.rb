77671fa8420fbb4345bcff16e353c3c769d5c137
earthctzn <earthctzn1@gmail.com>
2019-08-12 00:12:50 -0400

    Connecting CSS & a placeholder log in form in views/index.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < Sinatra::Base
 
+    configure do
+        set :views, 'app/views'
+        set :public_folder, 'public'
+    end
+
     get '/' do
-        "Hello World"
+        erb :index
     end
 
 
