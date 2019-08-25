664e22d62f30222549fbb85fd4b6c1c8f9fba1ca
earthctzn <earthctzn1@gmail.com>
2019-08-25 18:08:31 -0400

    Seeds.rb seeding places.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
-usr1 = User.create(username: "Morpheus", password: "bluepill")
-usr2 = User.create(username: "Micah", password: "thor")
-usr3 = User.create(username: "Caleb", password: "test")
-usr4 = User.create(username: "Bonny", password: "bark")
 
-story1 = Story.create(title: "Shwifty times", content: "Super schwifty ipsum lormen artic daft drunken fart", story_date: "June 1980", user: usr1)
-story2 = Story.create(title: "Squanchy times", content: "Super schwifty ipsum lormen artic daft drunken fart", story_date: "June 1980", user: usr2)
-story3 = Story.create(title: "Wubba lubba times", content: "Super schwifty ipsum lormen artic daft drunken fart", story_date: "June 2008", user: usr3)
-story4 = Story.create(title: "A boogaloo time", content: "Super schwifty ipsum lormen artic daft drunken fart", story_date: "June 2001", user: usr4)
-
-plc1 = Place.create(city: "Phoenix", state: "Arizona", country: "USA")
-plc2 = Place.create(city: "Kopenhagen", state: "Kopenhagen", country: "Demark")
-plc3 = Place.create(city: "Barcelona", state: "Cataluña", country: "Spain")
-plc4 = Place.create(city: "Valencia", state: "Cataluña", country: "Spain")
-
-story1.places << plc1
-story2.places << plc2
-story3.places << plc3
-story4.places << plc4
+Place.create(city: "Phoenix", state: "Arizona", country: "USA")
+Place.create(city: "Copenhagen", state: "Copenhagen", country: "Demark")
+Place.create(city: "Barcelona", state: "Cataluña", country: "Spain")
+Place.create(city: "Valencia", state: "Cataluña", country: "Spain")
+Place.create(city: "Madagascar", state: "Madagascar", country: "Madagascar")
+Place.create(city: "Tokyo", state: "Tokyo", country: "Japan")
